a85f9b16ac34a1551ed2ceaa08593c1f56929407
Alex Rossiter <asscott3@asu.edu>
2020-10-27 22:47:47 -0400

    Remove catch body

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -208,10 +208,6 @@ class WebServer {
 				num1 = Integer.parseInt(query_pairs.get("num1"));
 				num2 = Integer.parseInt(query_pairs.get("num2"));
 			} catch (NumberFormatException e) {
-				builder.append("HTTP/1.1 400 Bad Request\n");
-				builder.append("Content-Type: text/html; charset=utf-8\n");
-				builder.append("\n");
-				builder.append("Invalid Input");
 			}
 
 			// do math
